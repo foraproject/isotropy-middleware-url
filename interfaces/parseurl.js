e1b7ad3e5c03c2c9ff8328d79c5e79186826516d
Jeswin Kumar <jeswinpk@agilehead.com>
2016-02-26 19:35:16 +0530

    Fix annotations

--- interfaces/parseurl.js
+++ interfaces/parseurl.js
@@ -1,5 +1,5 @@
 import type { IncomingMessage } from "http";
 
 declare module "parseurl" {
-  declare function exports(req: IncomingMessage) : void;
+  declare function exports(req: IncomingMessage) : { href: string, pathname: string, query: string, search: string };
 }
